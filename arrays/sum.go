a438ebd2e475b4ada4fa5ac30d88216129012e51
suryxks <suryasuresh9876543@gmail.com>
2024-01-25 15:49:03 +0530

    arrays and slices

--- arrays/sum.go
+++ arrays/sum.go
@@ -7,3 +7,26 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var ans []int
+	for _, numbers := range numbersToSum {
+		ans = append(ans, Sum(numbers))
+	}
+
+	return ans
+}
+func SumAllTails(numbersToSum ...[]int) []int {
+	var ans []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			ans = append(ans, 0)
+		} else {
+			tail := numbers[1:]
+			ans = append(ans, Sum(tail))
+		}
+
+	}
+	return ans
+}
