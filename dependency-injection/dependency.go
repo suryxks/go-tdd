f4d743012f3f1481a77cfb14e58c0c5cbe7230a3
suryxks <suryasuresh9876543@gmail.com>
2024-01-29 17:28:48 +0530

    refactor mocking

--- dependency-injection/dependency.go
+++ dependency-injection/dependency.go
@@ -14,6 +14,8 @@ func Greet(writer *bytes.Buffer, name string) {
 
 const finalWord = "Go!"
 const countdownStart = 3
+const sleep = "sleep"
+const write = "write"
 
 type Sleeper interface {
 	Sleep()
@@ -22,8 +24,35 @@ type SpySleeper struct {
 	Calls int
 }
 
+type SpyCountdownOperations struct {
+	Calls []string
+}
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+
+	s.Calls = append(s.Calls, write)
+	return
+}
+
 func (s *SpySleeper) Sleep() {
 	s.Calls++
+}
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
 }
 
 func CountDown(writer io.Writer, sleeper Sleeper) {
@@ -35,13 +64,7 @@ func CountDown(writer io.Writer, sleeper Sleeper) {
 
 }
 
-type DefaultSleeper struct{}
-
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
-}
-
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	CountDown(os.Stdout, sleeper)
 }
