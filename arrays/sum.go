0ad3bcc61a1f9cd7e76e736b9a5330f027312df8
suryxks <suryasuresh9876543@gmail.com>
2024-02-16 16:19:10 +0530

    genenrics

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,11 +1,16 @@
 package main
 
+type Transaction struct {
+	From string
+	To   string
+	Sum  float64
+}
+
 func Sum(numbers []int) int {
-	sum := 0
-	for _, number := range numbers {
-		sum += number
+	add := func(acc, x int) int {
+		return acc + x
 	}
-	return sum
+	return Reduce[int](numbers, add, 0)
 }
 
 func SumAll(numbersToSum ...[]int) []int {
@@ -17,16 +22,36 @@ func SumAll(numbersToSum ...[]int) []int {
 	return ans
 }
 func SumAllTails(numbersToSum ...[]int) []int {
-	var ans []int
-
-	for _, numbers := range numbersToSum {
-		if len(numbers) == 0 {
-			ans = append(ans, 0)
+	sumTail := func(acc, x []int) []int {
+		if len(x) == 0 {
+			return append(acc, 0)
 		} else {
-			tail := numbers[1:]
-			ans = append(ans, Sum(tail))
+			tail := x[1:]
+			return append(acc, Sum(tail))
 		}
-
 	}
-	return ans
+	return Reduce[[]int](numbersToSum, sumTail, []int{})
+}
+
+func Reduce[T any, B any](collection []T, accumulator func(B, T) B, initialValue B) B {
+	var result = initialValue
+	for _, x := range collection {
+		result = accumulator(result, x)
+	}
+	return result
+}
+
+func BalanceFor(transactions []Transaction, name string) float64 {
+	var balance float64
+
+	adjustBalance := func(currentBalnce float64, t Transaction) float64 {
+		if t.From == name {
+			return currentBalnce - t.Sum
+		}
+		if t.To == name {
+			return currentBalnce + t.Sum
+		}
+		return currentBalnce
+	}
+	return Reduce(transactions, adjustBalance, balance)
 }
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -49,3 +49,27 @@ func TestSumTails(t *testing.T) {
 		checkSums(t, got, want)
 	})
 }
+
+func AssertEqual[T comparable](t *testing.T, got, want T) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %v ,want %v", got, want)
+	}
+}
+
+func TestBadBank(t *testing.T) {
+	transactions := []Transaction{
+		{
+			From: "Chris",
+			To:   "Riya",
+			Sum:  100,
+		}, {
+			From: "Adil",
+			To:   "Chris",
+			Sum:  25,
+		},
+	}
+	AssertEqual(t, BalanceFor(transactions, "Riya"), 100)
+	AssertEqual(t, BalanceFor(transactions, "Chris"), -75)
+	AssertEqual(t, BalanceFor(transactions, "Adil"), -25)
+}
