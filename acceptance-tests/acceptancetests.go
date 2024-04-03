8eed795d0afa3bce2ad86c5ca0b9320a5958e5ad
suryxks <suryasuresh9876543@gmail.com>
2024-04-03 12:41:06 +0530

    acceptance test intro

---
+++ acceptance-tests/acceptancetests.go
@@ -0,0 +1,84 @@
+package acceptancetests
+
+import (
+	"fmt"
+	"math/rand"
+	"net"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"syscall"
+	"time"
+)
+
+// acceptance tests are a kinf of "black box test aslo called as "Functional tests""
+// They should exercise the system as a user of the system would
+
+const baseBinName = "temp-testbinary"
+
+func LaunchTestProgram(port string) (cleanup func(), sendInterrupt func() error, err error) {
+	binName, err := buildBinary()
+	if err != nil {
+		return nil, nil, err
+	}
+	sendInterrupt, kill, err := runServer(binName, port)
+
+	cleanup = func() {
+		if kill != nil {
+			kill()
+		}
+		os.Remove(binName)
+	}
+	if err != nil {
+		cleanup()
+		return nil, nil, err
+	}
+	return cleanup, sendInterrupt, nil
+}
+
+func buildBinary() (string, error) {
+	binName := randomString(10) + "-" + baseBinName
+	build := exec.Command("go", "build", "-o", binName)
+	if err := build.Run(); err != nil {
+		return "", fmt.Errorf("cannot build tool %s %s", binName, err)
+	}
+	return binName, nil
+}
+func randomString(n int) string {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	s := make([]rune, n)
+	for i := range s {
+		s[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(s)
+}
+func runServer(binName string, port string) (sendInterrupt func() error, kill func(), err error) {
+	dir, err := os.Getwd()
+	if err != nil {
+		return nil, nil, err
+	}
+	cmdPath := filepath.Join(dir, binName)
+	cmd := exec.Command(cmdPath)
+	if err := cmd.Start(); err != nil {
+		return nil, nil, fmt.Errorf("cannot run temp converter: %s", err)
+	}
+	kill = func() {
+		_ = cmd.Process.Kill()
+	}
+	sendInterrupt = func() error {
+		return cmd.Process.Signal(syscall.SIGTERM)
+	}
+	err = waitForServerListening(port)
+
+}
+func waitForServerListening(port string) error {
+	for i := 0; i < 30; i++ {
+		conn, _ := net.Dial("tcp", net.JoinHostPort("localhost", port))
+		if conn != nil {
+			conn.Close()
+			return nil
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+	return fmt.Errorf("nothing seems to be listening on localhost:%s", port)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-tdd
 
 go 1.21.5
+
+require github.com/quii/go-graceful-shutdown v0.6.0
